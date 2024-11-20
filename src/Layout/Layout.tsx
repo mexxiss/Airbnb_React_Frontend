e15fcca3019e42364d0adc1ef504519a3696f47d
jagjit-mexxiss-dev <jagjit.singh.mexxiss@gmail.com>
2024-11-20 18:12:20 +0530

    implemented home-content api

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,9 +1,33 @@
-
 import { Outlet } from "react-router-dom";
 import Header from "../Components/Header/Header.jsx";
 import Footer from "../Components/Footer/Footer.jsx";
+import { useDispatch } from "react-redux";
+import { useQuery } from "@tanstack/react-query";
+import { IHomeContentData } from "../types/homeContentTypes.js";
+import { fetchHomeContent } from "../services/apiServices.js";
+import { useEffect } from "react";
+import { setHomeContent } from "../store/features/homeContentSlice.js";
+
+interface IResp {
+  success: boolean | null;
+  data: IHomeContentData | null;
+  statusCode: number | null;
+  message: string | null;
+}
 
 const Layout = () => {
+  const dispatch = useDispatch();
+  const { isLoading, isError, error, data } = useQuery<IResp>({
+    queryKey: ["homeContent"], // Query key to uniquely identify the query
+    queryFn: fetchHomeContent, // Function to fetch data
+  });
+  const finalData = data?.data;
+  useEffect(() => {
+    dispatch(setHomeContent(finalData as IHomeContentData));
+  }, [data]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError && error instanceof Error) return <p>Error: {error.message}</p>;
   return (
     <div className="max-w-[1900px] mx-auto">
       <Header />
